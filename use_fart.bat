bfaf180f27603ec0422732b05860d6619cdedc58
Alex Dyba <admin@vpixle.com>
2012-05-01 14:53:19 +0200

    use of fart.exe

---
+++ use_fart.bat
@@ -0,0 +1,3 @@
+fart -r *.html index-2.html index.html
+fart -r -f  *.html index-2.html index.html
+fart -r -C --remove *.html "\x3C!-- Added by HTTrack --\x3E\x3Cmeta http-equiv=\"content-type\" content=\"text\x2Fhtml;charset=UTF-8\" \x2F\x3E\x3C!-- \x2FAdded by HTTrack --\x3E"
